9d4326ba07db2050d923446d32c22544d2d7c063
thesimonjiang <141334620+thesimonjiang@users.noreply.github.com>
2023-08-29 06:26:06 +0000

    Update DeleteBaseline.ps1

--- DeleteBaseline.ps1
+++ DeleteBaseline.ps1
@@ -2,8 +2,7 @@
 # Made by Simon Jiang. https://github.com/thesimonjiang
 
 # To make this script to work on your own system: you need to replace the directory paths 
-# in this script with that of your own. Replace all the instances of "YOUR_OWN_SYSTEM_PATH"
-# and your own baseline file name.
+# in this script with that of your own. Replace all the instances of "YOUR_OWN_SYSTEM_PATH".
 
 Function DeleteAlphaBaseIfExists () {
     $baselineExists = Test-Path -Path YOUR_OWN_SYSTEM_PATH\baseline-Alpha.txt
