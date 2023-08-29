2fb76233f8bdc22e42298273b167b1efcf48f66d
thesimonjiang <141334620+thesimonjiang@users.noreply.github.com>
2023-08-29 06:25:10 +0000

    Update DeleteBaseline.ps1

--- DeleteBaseline.ps1
+++ DeleteBaseline.ps1
@@ -1,18 +1,22 @@
 ﻿# Delete Baseline Function part for the Simpe FIM PowerShell Script
 # Made by Simon Jiang. https://github.com/thesimonjiang
 
+# To make this script to work on your own system: you need to replace the directory paths 
+# in this script with that of your own. Replace all the instances of "YOUR_OWN_SYSTEM_PATH"
+# and your own baseline file name.
+
 Function DeleteAlphaBaseIfExists () {
-    $baselineExists = Test-Path -Path .\baseline-Alpha.txt
+    $baselineExists = Test-Path -Path YOUR_OWN_SYSTEM_PATH\baseline-Alpha.txt
 
     if ($baselineExists) {
-        Remove-Item -Path .\baseline-Alpha.txt
+        Remove-Item -Path YOUR_OWN_SYSTEM_PATH\baseline-Alpha.txt
     }
 }
 
 Function DeleteBetaBaseIfExists () {
-    $baselineExists = Test-Path -Path .\baseline-Beta.txt
+    $baselineExists = Test-Path -Path YOUR_OWN_SYSTEM_PATH\baseline-Beta.txt
 
     if ($baselineExists) {
-        Remove-Item -Path .\baseline-Beta.txt
+        Remove-Item -Path YOUR_OWN_SYSTEM_PATH\baseline-Beta.txt
     }
 }
